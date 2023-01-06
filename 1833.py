09d12e9b19c3b0ccc96550e91c14af7351a23530
Huy Bui <williamhuybui@gmail.com>
2023-01-06 09:19:59 -0600

    Create 1833.py

---
+++ 1833.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        res = 0
+        for price in costs:
+            if price <= coins:
+                res+=1
+                coins -= price
+        return res
