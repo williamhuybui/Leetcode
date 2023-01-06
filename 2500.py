dd245bac8105cf985f298fd3cd5eaf450961cd36
Huy Bui <williamhuybui@gmail.com>
2023-01-06 14:53:28 -0600

    Create 2500.py

---
+++ 2500.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        res = 0
+        #Sort all the row
+        for i in range(len(grid)):
+            grid[i] = sorted(grid[i])
+        #Find sum of max of each column
+        for j in range(len(grid[0])):
+            res += max([grid[i][j] for i in range(len(grid))])
+        return res
