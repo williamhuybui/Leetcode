2856580ca2c67477a3e27321cdcf8c86bdadf2d7
Huy Bui <williamhuybui@gmail.com>
2023-01-23 12:16:21 -0600

    Update 2541.py

--- 2541.py
+++ 2541.py
@@ -21,3 +21,31 @@ class Solution:
                 return -1
             
             is_add, is_sub = False, False
+
+# Faster solution: 26/49 case
+class Solution:
+    def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        res = 0
+        c_add, c_sub = 0, 0
+        for i in range(len(nums1)):
+            if nums1[i] == nums2[i]:
+                continue
+            elif nums1[i] < nums2[i]:
+                while nums1[i] < nums2[i]:
+                    nums1[i] += k
+                    c_add += 1
+                if nums1[i] != nums2[i]:
+                    # print('add')
+                    return -1
+            elif nums1[i] > nums2[i]:
+                while nums1[i] > nums2[i]:
+                    nums1[i] -= k
+                    c_sub += 1
+                if nums1[i] != nums2[i]:
+                    # print('sub')
+                    return -1
+        if c_add != c_sub:
+            # print('!=', c_add, c_sub)
+            return -1
+        else:
+            return c_add
