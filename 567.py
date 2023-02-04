2ae46c75286204deb06454ff5a9444077cb69b22
Huy Bui <williamhuybui@gmail.com>
2023-02-03 20:52:46 -0500

    Update 567.py

--- 567.py
+++ 567.py
@@ -9,3 +9,13 @@ class Solution:
             if s2[i:i+l1] in s1_list:
                 return True
         return False
+
+#Better one using sorted O(nlog(n))
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1 = sorted(s1)
+        l1, l2 = len(s1), len(s2)
+        for i in range(l2-l1+1):
+            if s1 == sorted(s2[i:i+l1]):
+                return True
+        return False
