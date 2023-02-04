291b374b633b2f005a2c14822e7b27bbad6f6a20
Huy Bui <williamhuybui@gmail.com>
2023-02-03 20:48:00 -0500

    Create 567.py

---
+++ 567.py
@@ -0,0 +1,11 @@
+#Exceeding time solution
+
+from itertools import permutations
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_list = ["".join(List) for List in permutations(s1)]
+        l1, l2 = len(s1), len(s2)
+        for i in range(l2-l1+1):
+            if s2[i:i+l1] in s1_list:
+                return True
+        return False
