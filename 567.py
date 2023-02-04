4ea96e0a930926465bc09238a5c3402ac4f62d0a
Huy Bui <williamhuybui@gmail.com>
2023-02-03 20:56:39 -0500

    Update 567.py

--- 567.py
+++ 567.py
@@ -19,3 +19,24 @@ class Solution:
             if s1 == sorted(s2[i:i+l1]):
                 return True
         return False
+
+#Most efficient one
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1, s2):
+        A = [ord(x) - ord('a') for x in s1]
+        B = [ord(x) - ord('a') for x in s2]
+
+        target = [0] * 26
+        for x in A:
+            target[x] += 1
+
+        window = [0] * 26
+        for i, x in enumerate(B):
+            window[x] += 1
+            if i >= len(A):
+                window[B[i - len(A)]] -= 1
+            if window == target:
+                return True
+        return False
+            
